0fd9771463dfe00e0da194cbf06c4563917b91e5
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-05-15 16:48:42 +0300

    Base.urls and setting input created

--- base/urls.py
+++ base/urls.py
@@ -1,3 +1,7 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 from django.urls import path
 from django.conf.urls import url
 from . import views
@@ -7,3 +11,6 @@ urlpatterns = [
     url(r'^viewphoto', views.viewphoto, name='viewphoto'),
     url(r'^create', views.create, name='create'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
