95c1028b74b5394e5aae1206dab428965cd25b30
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-05-15 16:32:51 +0300

    models.py - category and photo class created

--- base/models.py
+++ base/models.py
@@ -2,11 +2,18 @@ from django.db import models
 
 # Create your models here.
 
-class Profile(models.Model):
-    name = models.CharField(max_length =30)
-    description = models.CharField(max_length =30)
-    location = models.CharField(max_length =30)
-    category = models.CharField(max_length =30)
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
 
     def __str__(self):
         return self.name
+
+
+class Photo(models.Model):
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True)
+    image = models.ImageField(null=False, blank=False)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.description
