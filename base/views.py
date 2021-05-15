b672729cc938997e1ec80a0fc116b9716c9e8f08
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-05-15 11:33:20 +0300

    Bootstrap installed and Base.html bootstraped

--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
+#from datetime import db
+
 from django.shortcuts import render
+from django.http  import HttpResponse , request , Http404
+
 
 # Create your views here.
+def base(request):
+    return render(request, 'base/base.html')
