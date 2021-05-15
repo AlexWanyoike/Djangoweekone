0875b435ac4d761ad71f6c13195bee75f8675169
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-05-15 13:00:49 +0300

    Migrations and Shell Database Installed

--- base/models.py
+++ base/models.py
@@ -7,3 +7,6 @@ class Profile(models.Model):
     description = models.CharField(max_length =30)
     location = models.CharField(max_length =30)
     category = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
