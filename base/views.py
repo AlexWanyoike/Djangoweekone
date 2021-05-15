7a51b716d635b5fc3fa854b3bf31aa4824a46689
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-05-15 17:18:38 +0300

    Various Accounts Categories Highlighted

--- base/views.py
+++ base/views.py
@@ -2,10 +2,12 @@
 
 from django.shortcuts import render
 from django.http  import HttpResponse , request , Http404
-
+from .models import Category , Photo
 
 # Create your views here.
 def base(request):
+    categories = Category.objects.all()
+    context = { 'categories' : categories }
     return render(request, 'base/base.html')
 
 def viewphoto(request):
