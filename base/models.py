540100bc777ad9cc5457a7343decc299ad833e38
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-05-15 21:53:44 +0300

    For loop created

--- base/models.py
+++ base/models.py
@@ -3,17 +3,31 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, null=False, blank=False)
+    name = models.CharField(max_length=100  )
 
     def __str__(self):
         return self.name
 
+class Location(models.Model):
+    name = models.CharField(max_length=100   )
+
+    def __str__(self):
+        return self.name
 
 class Photo(models.Model):
+    name = models.CharField(max_length=100   )
+
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True, blank=True)
-    image = models.ImageField(null=False, blank=False)
+        Category, on_delete=models.CASCADE  )
+
+    location = models.ForeignKey(
+        Location, on_delete=models.CASCADE  )
+    
+
+    image = models.ImageField(upload_to='media/')
+
     description = models.TextField()
 
     def __str__(self):
-        return self.description
+        return self.name
+
--- base/views.py
+++ base/views.py
@@ -6,14 +6,14 @@ from .models import Category , Photo
 
 # Create your views here.
 def base(request):
-    
 
     categories = Category.objects.all()
+    photo = Photo.objects.all()
     #context = {'categories': categories, 'photos': photos}
-    return render(request, 'base/base.html', {'categories' : categories})
+    return render(request, 'base/base.html', {'categories' : categories, 'photo': photo})
 
 def viewphoto(request, pk):
-    photo = photo.objects.get(id=pk)
+    photo = Photo.objects.get(id=pk)
     return render(request, 'base/photo.html', {'photo':photo})
 
 def create(request):
