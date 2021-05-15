e01de1c590183fafdbe39563d0d044bbb17715c6
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-05-15 15:33:07 +0300

    Create, base and photo html created

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from django.conf.urls import url
 from . import views
 
-urlpatterns=[
-    url('^$',views.base,name = 'base'),
+urlpatterns = [
+    url(r'^base', views.base, name='base'),
+    url(r'^photo', views.viewphoto, name='viewphoto'),
+    url(r'^create', views.create, name='create'),
 ]
--- base/views.py
+++ base/views.py
@@ -7,3 +7,9 @@ from django.http  import HttpResponse , request , Http404
 # Create your views here.
 def base(request):
     return render(request, 'base/base.html')
+
+def viewphoto(request):
+    return render(request, 'base/photo.html')
+
+def create(request):
+    return render(request , 'base/create.html')
