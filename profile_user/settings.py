8b08af33cc8cf02e89254b19cb9b5a69ec37e92f
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-05-15 12:28:46 +0300

    Postgres and Database Created

--- profile_user/settings.py
+++ profile_user/settings.py
@@ -84,6 +84,14 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'profile_pic',
+        'USER': 'alex',
+    'PASSWORD':'roverson3',
+    }
+}
 
 
 # Password validation
@@ -110,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
