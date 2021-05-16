3bf4b93d9d4c5bb46b753762e375660deabdd7db
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-05-16 19:48:46 +0300

    Filter Bu Location html Created

--- base/views.py
+++ base/views.py
@@ -1,14 +1,19 @@
 #from datetime import db
 
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http  import HttpResponse , request , Http404
 from .models import Category , Photo , Location
 
 # Create your views here.
 def base(request):
 
+    category = request.GET.get('category')
+    if category == None:
+        photo = Photo.objects.all()
+    else:
+        photo = Photo.objects.filter(category__name=category)
+
     categories = Category.objects.all()
-    photo = Photo.objects.all()
     context = {'categories': categories, 'photo': photo}
     return render(request, 'base/base.html', context)
 
@@ -19,15 +24,6 @@ def viewphoto(request, pk):
 def create(request):
     categories = Category.objects.all()
     locations = Location.objects.all()
-
-    for photo in photos:
-            photo = Photo.objects.create(
-                category=category,
-                description=data['description'],
-                photo=photo,
-            )
-
-    return redirect('base')
 
     context = {'categories': categories, 'locations': locations}
     return render(request , 'base/create.html', context)
