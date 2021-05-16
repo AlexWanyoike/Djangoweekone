944533662637a3594e5a7144a7c7db20f156f9de
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-05-16 10:28:05 +0300

    Create method categories and locations inputted

--- base/views.py
+++ base/views.py
@@ -2,19 +2,32 @@
 
 from django.shortcuts import render
 from django.http  import HttpResponse , request , Http404
-from .models import Category , Photo
+from .models import Category , Photo , Location
 
 # Create your views here.
 def base(request):
 
     categories = Category.objects.all()
     photo = Photo.objects.all()
-    #context = {'categories': categories, 'photos': photos}
-    return render(request, 'base/base.html', {'categories' : categories, 'photo': photo})
+    context = {'categories': categories, 'photo': photo}
+    return render(request, 'base/base.html', context)
 
 def viewphoto(request, pk):
     photo = Photo.objects.get(id=pk)
     return render(request, 'base/photo.html', {'photo':photo})
 
 def create(request):
-    return render(request , 'base/create.html')
+    categories = Category.objects.all()
+    locations = Location.objects.all()
+
+    for photo in photos:
+            photo = Photo.objects.create(
+                category=category,
+                description=data['description'],
+                photo=photo,
+            )
+
+    return redirect('base')
+
+    context = {'categories': categories, 'locations': locations}
+    return render(request , 'base/create.html', context)
